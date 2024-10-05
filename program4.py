7059564473f8280f94197244459efe60ab203868
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 23:40:24 +0530

    conditional statements code added

---
+++ program4.py
@@ -0,0 +1,44 @@
+marks = int(input("Enter the marks you got "))
+
+if(marks >=90):
+    print("A")
+elif(marks<90 and marks>=80):
+    print("B")
+elif(marks<80 and marks>=70):
+    print("B")
+else:
+    print("D")    
+
+
+# nesting if statement
+age = 34
+if(age>18):
+    if(age>80):
+        print("cannot Drive")
+    else :
+        print("can Drive")
+else:
+    print("cannot Drive")
+
+
+# check a number entered by user is even or odd
+
+num =int(input("ENter a number to be checked "))
+if(num%2==0):
+    print("num is a even number")
+else:
+    print("num is a odd number ")
+    
+
+# find the  largest of three numbers
+
+num1 =int(input("ENter the 1st number"))
+num2 =int(input("ENter the 2nd number"))
+num3 =int(input("ENter the 3rd number"))
+
+if(num1 > num2 and num1 >num3):
+    print(num1," is Greatest")
+elif(num2 > num1 and num2 >num3):
+    print(num2," is Greatest")
+else:
+    print(num3," is Greatest")
