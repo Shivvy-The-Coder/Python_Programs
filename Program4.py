fd7bfc1e45274e4c11dfbf3f272175f77215cf9b
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 23:51:05 +0530

    conditonal statement code added

--- Program4.py
+++ Program4.py
@@ -21,7 +21,7 @@ else:
     print("cannot Drive")
 
 
-# check a number entered by user is even or odd
+#WAP check a number entered by user is even or odd
 
 num =int(input("ENter a number to be checked "))
 if(num%2==0):
@@ -30,11 +30,11 @@ else:
     print("num is a odd number ")
     
 
-# find the  largest of three numbers
+# WAP to find the  largest of three numbers
 
 num1 =int(input("ENter the 1st number"))
 num2 =int(input("ENter the 2nd number"))
-num3 =int(input("ENter the 3rd number"))
+num3 =int(input("ENter the 3rd number "))
 
 if(num1 > num2 and num1 >num3):
     print(num1," is Greatest")
@@ -42,3 +42,11 @@ elif(num2 > num1 and num2 >num3):
     print(num2," is Greatest")
 else:
     print(num3," is Greatest")
+
+
+# WAP program to check wether a number is multiple of 7 or not
+num = int (input("Enter the number to be checked "))
+if(num%7==0):
+    print(num," is a multiple of 7")
+else:
+    print(num," is not a multiple of 7")
