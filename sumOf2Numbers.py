129ab969ad804777f663aa8bf68feb3bfbd9429d
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 20:07:22 +0530

    codes updated

---
+++ sumOf2Numbers.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter number 1 "))
+n2 = int(input("Enter number 2 ")
+)
+print(type(n1), type(n2))
+
+print(n1+n2)
+print("Sum of n1 and n2 will be ", n1+n2)
+print("power of the resulting sum to the power of 2 wil be ", (n1+n2)**2)
