3844f35d5616165e60aaa59d7130dbee8589485b
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 20:32:01 +0530

    name of the file has been changed to program1

--- areaOfSquare.py
+++
@@ -1,6 +0,0 @@
-side = float(input ("Enter the side of square "))
-print("Area of square will be ", side*side)
-
-inp1=float(input("Enter the inp1 "))
-inp2=float(input("Enter the inp2 "))
-print("Average of inp1 and inp2 is ",(inp1+inp2)/2)
