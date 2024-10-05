88e38ff499f09644c50c38b6430acbac693893d6
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 19:52:20 +0530

    first two programs added

---
+++ firstProgram.py
@@ -0,0 +1,34 @@
+print("Shiva is my name ")
+print("my age is 23")
+# above codes will print both the lines separetly
+
+print("Shivam is my name" , " my age is 23")
+# here the above line will use only one print fucntion to print the two statements together in a 
+# a single line  with a space in between them. using comma  as a separator. .
+
+
+print("Hello This is my first Program in Python")
+
+print(23)
+# printing a number in python
+print(23+35)
+
+
+name="Shivam "
+age =23
+price =25.99
+print("My name is ",name)
+print("My age is ", age)
+print(name , age , price)
+
+# varibale naem does not starts with a digit in pyton , and identifier
+# can be of any length
+
+print(type(age))
+print(type(name))
+print(type(price))
+
+check =True
+while(check):
+        print("Hello")
+        check=False
