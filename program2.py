665243831b5e15ee26aa6d707dd9b4cec66a7c8a
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-05 20:35:29 +0530

    codes added

---
+++ program2.py
@@ -0,0 +1,15 @@
+# area of square
+side = float(input ("Enter the side of square "))
+print("Area of square will be ", side*side)
+
+
+# average of two float values
+inp1=float(input("Enter the inp1 "))
+inp2=float(input("Enter the inp2 "))
+print("Average of inp1 and inp2 is ",(inp1+inp2)/2)
+
+# print true if 1st number is greater than 2nd
+
+inp1=float(input("Enter the inp1 "))
+inp2=float(input("Enter the inp2 "))
+print(inp1>inp2)
