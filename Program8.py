13a1d88427318a4b2d07deab90a3dfe8eddd5537
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-06 14:52:15 +0530

    Some basics coding on dictionary and set has been done herer

--- Program8.py
+++ Program8.py
@@ -23,9 +23,32 @@ set1.add("python")
 set1.add("JavaScript")
 set1.add("Java")
 set1.add("python")
-set1.add("java")
+set1.add("Java")
 set1.add("C++")
 set1.add("C")
 
 print(len(set1)) 
 # This will work correctly now
+
+
+
+# WAP to enter marks of 3 subjects from the usr and store them ina dictionary . tart with  an empty d
+# dicitonary and add  one by one. use subject name as key and mark as value
+
+subject ={}
+subject.update({"math":90})
+subject.update({"english":70})
+subject.update({"COA":99})
+
+print(subject)
+
+#  figure a way to store 9 and 9.0 as separate value in the set
+vals= set()
+vals.add(9)
+vals.add(float(9.0))
+print(vals)
+
+vals= set()
+tup=(9,9.0)
+vals.add(tup)
+print(vals)
