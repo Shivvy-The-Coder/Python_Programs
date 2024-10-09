a66895b2e5c7d7df0ddcf82be457ef0291f077f4
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-09 11:03:10 +0530

    codes added

--- Questions/q3.py
+++ Questions/q3.py
@@ -17,5 +17,21 @@ print(name)
 
 
 # here we can see that we have declared len =5 where as len is builtin fcuntion that we are using
-
+len=10
 print(len)
+
+
+
+# we shoould avoid changing any global data directly it increases the risk of namespacing 
+#  therer we can us ethe below methidi which mnimizes the risk of these factors
+
+#  we can use return to change any gobal fata we want 
+x=1000
+def change ():
+    return 100
+
+print("before calling the ficntion change ",x)
+x=change()
+print("After calling the function change",x)
+
+
