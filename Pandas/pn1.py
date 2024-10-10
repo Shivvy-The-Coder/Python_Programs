298a6138e1f5824ba82b1c92654fd66952496cec
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-11 00:41:42 +0530

    python pandas fucntion working added

--- Pandas/pn1.py
+++ Pandas/pn1.py
@@ -1,20 +1,43 @@
 import pandas as pd
 import numpy as np
 
+# storing a directory of csv file in variable path  make sure to use 'r' before the path 
 path=r"E:\PYthon Programming Apna College\Pandas\CSV\NY_weather.csv"
-
+# now using that directory to read the csv file 
 df = pd.read_csv(path)
-
+# printing the csv file which now can be accesed with the help if variable df
 print(df)
 
-# how to extrat a single coloumn of  data from the dataframe
-c1=df.EST
-print(c1)
 
+"""# HOW TO ACCES A CSV FILE FROM A CSV FILE"""
+# method 1
+c1=df.EST 
+# print(c1)
+print("EST column has been printed and now we will print Temperature")
 c2=df.Temperature
 print(c2)
 
-print(df.head(20))
+# method 2
+c1=df['EST']
+print(c1)
+c2=df["Temperature"]
+print(c2)
+print(df[['EST',"Temperature"]])
+
+c1_c2 = df[['EST', 'Temperature']]
+
+print("EST and Temperature columns together:")
+print(c1_c2)
 
 
-print(df.EST , df.Temperature)
+#  combining two coloumns and resulting into a 3rd column 
+EST_TEmp = df['EST'].astype(str) + df ['Temperature'].astype(str)
+print(EST_TEmp)
+
+# how to rename a coloumns in pandas using  rename() function
+
+df.rename(columns={'EST':"DATE" , "Temperature":"temp"},inplace=True)
+print(df)
+
+
+#
