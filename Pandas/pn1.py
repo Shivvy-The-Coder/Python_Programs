1ad08fc264c58d8d0d85df7430784275cbc4dd6b
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-11 02:46:58 +0530

    some basic fucntion of pandas has been added

--- Pandas/pn1.py
+++ Pandas/pn1.py
@@ -5,9 +5,13 @@ import numpy as np
 path=r"E:\PYthon Programming Apna College\Pandas\CSV\NY_weather.csv"
 # now using that directory to read the csv file 
 df = pd.read_csv(path)
+df2=pd.read_csv(path)
+print(df2)
 # printing the csv file which now can be accesed with the help if variable df
 print(df)
 
+df['temp']=df2['Temperature']
+print(df)
 
 """# HOW TO ACCES A CSV FILE FROM A CSV FILE"""
 # method 1
@@ -45,6 +49,55 @@ print(df)
 df['temp + 3 degrees']=df['temp'].apply(lambda x:x+3)
 # print(df["temp + 3 degrees"])
 print(df)
+
 # method2
 df['temp+20']=df['temp']**2
 print(df)
+
+#finding sum , max , min, median , mean , count etc
+maxTemp = df['temp'].max()
+minTemp = df['temp'].min()
+meanTemp = df['temp'].mean()
+medianTemp=df['temp'].median()
+sumTemp=df['temp'].sum()
+
+print("Performing aggregate function on these are ")
+print("maxtemp",maxTemp)
+print("mintemp",minTemp)
+print("meantemp",meanTemp)
+print("mediantemp",medianTemp)
+print("sumtemp",sumTemp)
+
+# filterng rows based on coloumn values
+
+Temp_Greater_30 = df[df['temp']>30]
+print(df)
+
+
+# sorting the data based on  a coloumn value
+
+df_sorted = df.sort_values(by='temp' , ascending=False)
+print(df_sorted)
+
+total = df['temp'].count()
+print(total)
+
+df_sorted_asc=df.sort_values(by='temp',ascending=True)
+print(df_sorted_asc)
+
+# df=df.drop('temp', axis=0)
+# print(df)
+
+# i dropped coloumn temp , but i regained it by creating another data fram from th main csv file
+#  and then adding the temperatur col as name temp in df  from df2
+#  like this 
+# df['temp']=df2['Temperature']
+
+
+# how to check for null values i python df
+
+check_null_values_in_temp = df['temp'].isnull()
+print(check_null_values_in_temp)
+
+
+
