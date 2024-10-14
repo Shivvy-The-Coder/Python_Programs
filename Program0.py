585670ed604af4dff689e1d3ad2cc693d24e8001
Shivvy-The-Coder <krshivam455@gmail.com>
2024-10-14 23:59:53 +0530

    added

--- Program0.py
+++ Program0.py
@@ -4,5 +4,5 @@ n2 = int(input("Enter number 2 ")
 print(type(n1), type(n2))
 
 print(n1+n2)
-print("Sum of n1 and n2 will be ", n1+n2)
+print("S  um of n1 and n2 will be ", n1+n2)
 print("power of the resulting sum to the power of 2 will be this -> ", (n1+n2)**2)
